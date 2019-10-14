f86352389bddc3483838263e51d497f2d86de299
Victor J. Pudelski <vjpudelski@gmail.com>
2019-10-14 07:52:59 -0400

    vanilla js routing

---
+++ scripts/router/router.js
@@ -0,0 +1,63 @@
+'use strict';
+
+function Router(routes, rootElem){
+  try{
+    if (!routes) {
+      throw 'eror: need to know where to go and how to get there';
+    }
+
+    this.constructor (routes, rootElem);
+    this.init();
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+Router.prototype = {
+  routes: undefined, 
+  rootElem: undefined,
+  constructor: function (routes, rootElem) {
+    this.routes = routes;
+    this.rootElem = document.getElementById(rootElem);
+  },
+  init: function () {
+    var r = this.routes;
+    (function (scope, r) {
+      window.addEventListener('hashchange', (e) => {
+        scope.hasChanged(scope, r);
+      });
+    })(this, r);
+    this.hasChanged(this, r);
+  },
+  hasChanged: function (scope, r) {
+    if (window.location.hash.length > 0) {
+      for(var i = 0, length = r.length; i < length; i++){
+        var route = r[i];
+        if (route.isActiveRoute(window.location.hash.substr(1))) {
+          scope.goToRoute(route.htmlName);
+        }
+      }
+    } else {
+      for (var i = 0, length = r.length; i < length; i++) {
+        var route = r[i];
+        if (route.default) {
+          scope.goToRoute(route.htmlName);
+        }
+      }
+    }
+  },
+  goToRoute: function (htmlName) {
+    (function(scope) {
+      var url = './views/' + htmlName, 
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          scope.rootElem.innerHTML = this.responseText;
+        }
+      };
+
+      xhttp.open('GET', url, true);
+      xhttp.send();
+    })(this);
+  }
+}
